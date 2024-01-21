aec90e02dfad6f0baabd286ecc6c8235eeb5e58b
Luna Davis <ddavi142@asu.edu>
2024-01-21 05:37:51 -0700

    Update it, make it better

--- js/dynamic-text.js
+++ js/dynamic-text.js
@@ -1,17 +1,19 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  const contentSections = document.querySelectorAll('.content');
-  console.log(contentSections)
+window.addEventListener("DOMContentLoaded", (event) => {
+  const contentSections = document.querySelectorAll(".content");
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.style.animation = 'bounceIn 1s forwards';
-        entry.target.style.visibility = 'visible';
-      } else {
-        entry.target.style.animation = 'none';
-      }
-    });
-  }, { threshold: 0.1 });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.style.animation = "bounceIn 1s forwards";
+          entry.target.style.visibility = "visible";
+        } else {
+          entry.target.style.animation = "none";
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
 
   contentSections.forEach((section) => {
     observer.observe(section);
